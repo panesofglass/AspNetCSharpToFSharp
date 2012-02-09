86eb408f0cadf5b49cd60f029e2c0fa9c14acec4
panesofglass <ryan.riley@panesofglass.com>
2012-02-09 06:54:00 -0800

    Add F# MVC project.

---
+++ src/FSharp.Mvc/FSharp.MvcWebApp/HomeController.fs
@@ -0,0 +1,9 @@
+﻿namespace MsdnWeb.Controllers
+
+open System.Web
+open System.Web.Mvc
+
+type HomeController() =
+    inherit Controller()
+    member this.Index () =
+        this.View() :> ActionResult
